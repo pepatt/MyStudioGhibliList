c6d745c4dc621579d25e2bcfe3939e52d951f125
Tymur Rezaei <rezai.timur.04@gmail.com>
2024-11-13 12:10:38 -0800

    start development of home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Cards from './components/Cards/Cards'
 import Login from './components/Login/Login'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <BrowserRouter>
      <Routes>
-        <Route path='/' element = {<Cards />} />
+        <Route path='/' element = {<Home />} />
+        <Route path='/ghibli' element = {<Cards />} />
         <Route path='/login' element = {<Login />} />
      </Routes>
     </BrowserRouter>
